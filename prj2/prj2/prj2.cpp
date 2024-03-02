c310a33f04f4363bd2e0852bf4cd2388eb1bb293
qwerty2024 <multanmgu@gmail.com>
2024-03-02 13:39:46 +0300

    task B

---
+++ prj2/prj2/prj2.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string input;
+    std::getline(std::cin, input, ':');
+    int a = stoi(input);
+
+    std::getline(std::cin, input);
+    int b = stoi(input);
+    
+    std::getline(std::cin, input, ':');
+    int c = stoi(input);
+    
+    std::getline(std::cin, input);
+    int d = stoi(input);
+    
+    std::getline(std::cin, input);
+    int match = stoi(input);
+
+    //std::cout << std::endl << a << " " << b << " " << c << " " << d << " " << e << std::endl;
+
+    int count1 = a + c;
+    int count2 = b + d;
+
+    if (count1 > count2)
+    {
+        std::cout << 0;
+    }
+    else if (count1 < count2)
+    {
+        int offset = count2 - count1;
+        c += offset;
+        if (match == 1)
+        {
+            if (c > b)
+            {
+                std::cout << offset;
+            }
+            else
+            {
+                std::cout << offset + 1;
+            }
+        }
+        else if (match == 2)
+        {
+            if (a > d)
+            {
+                std::cout << offset;
+            }
+            else
+            {
+                std::cout << offset + 1;
+            }
+        }
+    }
+    else // count1 = count2
+    {
+        if (match == 1)
+        {
+            if (c > b)
+            {
+                std::cout << 0;
+            }
+            else
+            {
+                std::cout << 1;
+            }
+        }
+        else if (match == 2)
+        {
+            if (a > d)
+            {
+                std::cout << 0;
+            }
+            else
+            {
+                std::cout << 1;
+            }
+        }
+        else std::cout << "error" << std::endl;
+    }
+}
