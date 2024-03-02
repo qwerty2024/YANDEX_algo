45a1461a8e00dd50998d2166f5743268db5d65d7
qwerty2024 <multanmgu@gmail.com>
2024-03-02 17:45:19 +0300

    task C

---
+++ prj3/prj3/prj3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string input;
+    std::getline(std::cin, input);
+    int n = stoi(input);
+
+    unsigned long long res = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::getline(std::cin, input);
+        int tmp = stoi(input);
+
+        res += tmp / 4 + tmp % 4 + ((tmp % 4) == 3 ? -1 : 0);
+    }
+
+    std::cout << res << std::endl;
+
+    return 0;
+}
